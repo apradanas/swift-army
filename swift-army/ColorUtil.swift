65e6b02debc0a8bbae64cc843fd6f267e5714f0e
apradanas <apradanas@gmail.com>
2015-05-21 00:25:21 +0700

    Support hex color string and int

--- swift-army/ColorUtil.swift
+++ swift-army/ColorUtil.swift
@@ -10,11 +10,55 @@ import Foundation
 
 public extension UIColor {
     
-    func colorFromHex(rgbValue: UInt32, alpha: Double = 1.0) -> UIColor {
-        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 256.0
-        let green = CGFloat((rgbValue & 0xFF00) >> 8) / 256.0
-        let blue = CGFloat(rgbValue & 0xFF) / 256.0
+    convenience init(rgba: String) {
+        var hex:   String = rgba
+        var red:   CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue:  CGFloat = 0.0
+        var alpha: CGFloat = 1.0
         
-        return UIColor(red: red, green: green, blue: blue, alpha: CGFloat(alpha))
+        if rgba.hasPrefix("#") {
+            let index = advance(rgba.startIndex, 1)
+            hex = rgba.substringFromIndex(index)
+        }
+        
+        let scanner = NSScanner(string: hex)
+        var hexValue: CUnsignedLongLong = 0
+        if scanner.scanHexLongLong(&hexValue) {
+            switch (count(hex)) {
+            case 3:
+                red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                blue  = CGFloat(hexValue & 0x00F)              / 15.0
+            case 4:
+                red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                alpha = CGFloat(hexValue & 0x000F)             / 15.0
+            case 6:
+                red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+            case 8:
+                red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+            default:
+                print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
+            }
+        } else {
+            println("Scan hex error")
+        }
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    convenience init(rgb: Int, alpha: CGFloat = 1.0) {
+        let red   = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8)  / 255.0
+        let blue  = CGFloat(rgb & 0x0000FF)         / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
 }
