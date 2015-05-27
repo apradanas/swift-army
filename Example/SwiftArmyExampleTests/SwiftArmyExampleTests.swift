2a06d6f439d1227bbe937e05a0e0849279ff30bc
apradanas <apradanas@gmail.com>
2015-05-27 22:12:59 +0700

    Add example project

---
+++ Example/SwiftArmyExampleTests/SwiftArmyExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftArmyExampleTests.swift
+//  SwiftArmyExampleTests
+//
+//  Created by Onebit-Adit-Mac on 5/27/15.
+//  Copyright (c) 2015 @apradanas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftArmyExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
