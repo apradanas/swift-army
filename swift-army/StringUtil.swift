40bbc1d2ac2ba7e92fb929f30a1abdaaaa36cca9
apradanas <apradanas@gmail.com>
2015-05-29 02:21:59 +0700

    Add string email validation

--- swift-army/StringUtil.swift
+++ swift-army/StringUtil.swift
@@ -47,6 +47,16 @@ public extension String {
         return trimLeft().trimRight()
     }
     
+    // MARK: - Validation
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        var emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(self)
+    }
+    
+    // MARK: - Conversion
+    
     func toDouble() -> Double? {
         let scanner = NSScanner(string: self)
         var double: Double = 0
