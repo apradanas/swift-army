d571fca77cb0783fd38615a2fb3ff1dea4f1c51f
apradanas <apradanas@gmail.com>
2015-05-31 23:27:01 +0700

    Add more methods

--- swift-army/DoubleUtil.swift
+++ swift-army/DoubleUtil.swift
@@ -9,6 +9,15 @@
 import Foundation
 
 public extension Double {
+
+	/**
+        Absolute value.
+    
+        :returns: absolute value of self
+    */
+    func abs() -> Double {
+        return Foundation.fabs(self)
+    }
     
     /**
         Rounds self to the smallest integer >= self.
@@ -26,5 +35,14 @@ public extension Double {
     */
     func floor() -> Double {
         return Foundation.floor(self)
+    }
+
+    /**
+        Squared root.
+    
+        :returns: squared root of self
+    */
+    func sqrt() -> Double {
+        return Foundation.sqrt(self)
     }
 }
